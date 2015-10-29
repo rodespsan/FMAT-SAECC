c8c4c0034e4dc666e58964faf5ff11720aaedb00
ricardo1121 <chigkim@prodigy.net.mx>
2015-10-29 10:35:25 -0600

    Borrar carpeta src
    Borra la carpeta con todos sus archivos

--- src/saecc/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=saecc',
-    'username' => 'rooter',
-    'password' => 'rooter',
-    'charset' => 'utf8',
-];
