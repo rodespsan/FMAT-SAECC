f7a93538a00588548b9662671ad89152487f2d7f
ricardo1121 <chigkim@prodigy.net.mx>
2015-11-04 00:48:40 -0600

    Client form + radiolist, dropdownlist y checkbox.
    Se agregó un radiolist al campo client_type_id; un dropdownlist a
    discipline_id al cual le falta tener la opción de no escoger ningún
    elemento o dejarlo vacío; y un checkbox a status al cual le falta estar
    seleccionado por defecto y mostrar bien la etiqueta.

--- src/saecc/views/client/_form.php
+++ src/saecc/views/client/_form.php
@@ -2,6 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\models\ClientType;
+use app\models\Discipline;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Client */
@@ -18,11 +21,22 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'last_name')->textInput(['maxlength' => 175]) ?>
 
-    <?= $form->field($model, 'client_type_id')->textInput(['maxlength' => 10]) ?>
+    <?= $form->field($model, 'client_type_id')->radioList(
+		ArrayHelper::map(
+			ClientType::find()->all(),
+			'id',
+			'type'))
+	?>
 
-    <?= $form->field($model, 'discipline_id')->textInput(['maxlength' => 10]) ?>
+    <?= $form->field($model, 'discipline_id')->dropDownList(
+		ArrayHelper::map(
+			Discipline::find()->all(),
+			'id',
+			'name', 'short_name'))
+	?>
 
-    <?= $form->field($model, 'status')->textInput() ?>
+    <?= $form->field($model, 'status')->checkbox([])->label('Activo'); ?>
+	
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
